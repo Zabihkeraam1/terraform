affc11a2a2d20c5906c65875056e3e6ed231a0a7
Zabih Keraam <keraamzabih@gmail.com>
2025-02-01 19:13:28 +0430

    First try for devops

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  console.log('Docker App Component is available');
+
+  return (
+    <>
+      
+      <h1>Hello DovOps</h1>
+    </>
+  )
+}
+
+export default App
