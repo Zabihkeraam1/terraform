9c9111eb504d62d56e7ca7d74e582f5f6d6ca9ef
Zabih Keraam <keraamzabih@gmail.com>
2025-03-11 19:47:05 +0430

    main.tf updated to check the security group

--- main.tf
+++ main.tf
@@ -49,9 +49,9 @@ resource "aws_security_group" "web_server_sg" {
 # locals {
 #   security_group_id = length(data.aws_security_group.existing_sg) > 0 ? data.aws_security_group.existing_sg.id : aws_security_group.web_server_sg[0].id
 # }
-locals {
-  security_group_id = length(data.aws_security_group.existing_sg.id) > 0 ? data.aws_security_group.existing_sg.id : aws_security_group.web_server_sg[0].id
-}
+# locals {
+#   security_group_id = length(data.aws_security_group.existing_sg.id) > 0 ? data.aws_security_group.existing_sg.id : aws_security_group.web_server_sg[0].id
+# }
 
 locals {
   security_group_id = data.aws_security_group.existing_sg.id
