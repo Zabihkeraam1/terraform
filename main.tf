3b1a24ad22d95183bd440a029b75e00a8c8bc87b
Zabih Keraam <keraamzabih@gmail.com>
2025-03-11 19:45:59 +0430

    main.tf updated to check the security group

--- main.tf
+++ main.tf
@@ -53,6 +53,10 @@ locals {
   security_group_id = length(data.aws_security_group.existing_sg.id) > 0 ? data.aws_security_group.existing_sg.id : aws_security_group.web_server_sg[0].id
 }
 
+locals {
+  security_group_id = data.aws_security_group.existing_sg.id
+
+}
 
 
 # Create an EC2 instance
