c3c364f3aa11e6e78f315f2664c76352ea1f65f4
Zabih Keraam <keraamzabih@gmail.com>
2025-03-16 22:31:55 +0430

    Key generation

--- main.tf
+++ main.tf
@@ -88,23 +88,42 @@ resource "aws_eip_association" "web_server_eip_assoc" {
   instance_id   = aws_instance.web_server.id
   allocation_id = aws_eip.web_server_eip.id
 }
+
+# Generate an SSH key pair
+resource "tls_private_key" "github_actions" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Save the private key to a file (optional, for debugging)
+resource "local_file" "private_key" {
+  content  = tls_private_key.github_actions.private_key_pem
+  filename = "id_rsa"
+}
+
+# Save the public key to a file (optional, for debugging)
+resource "local_file" "public_key" {
+  content  = tls_private_key.github_actions.public_key_openssh
+  filename = "id_rsa.pub"
+}
+
+# Create an AWS key pair
+resource "aws_key_pair" "github_actions" {
+  key_name   = "github-actions-key"
+  public_key = tls_private_key.github_actions.public_key_openssh
+}
 # Create an EC2 instance
 resource "aws_instance" "web_server" {
   ami             = "ami-02e2af61198e99faf"
   instance_type   = "t3.micro"
   security_groups = [aws_security_group.web_server_sg.name]
-  user_data     = <<-EOF
-                #cloud-config
-                users:
-                  - name: ubuntu
-                    ssh-authorized-keys:
-                      - ${file("~/.ssh/id_rsa.pub")}
-                EOF
+  key_name      = aws_key_pair.github_actions.key_name
   tags = {
     Name = "web-server"
   }
 
 }
+
 # Output the public IP of the EC2 instance
 # output "instance_public_ip" {
 #   value = aws_instance.web_server.public_ip
@@ -116,6 +135,12 @@ resource "null_resource" "delay" {
   provisioner "local-exec" {
     command = "sleep 10"
   }
+}
+
+# Output the private key
+output "private_key" {
+  value     = tls_private_key.github_actions.private_key_pem
+  sensitive = true  # Mark the output as sensitive to avoid logging
 }
 output "instance_public_ip" {
   value = aws_eip.web_server_eip.public_ip
